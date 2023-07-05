20e9dee8ecfb107fbc71fe729be34b0b62154be4
Fyodor Danilov <danilovfa@protonmail.com>
2023-07-05 17:23:05 +0300

    Add mappers

---
+++ app/src/main/java/com/danilovfa/cryptocurrencies/data/local/mapper/CryptocurrencyItemEntityMapper.kt
@@ -0,0 +1,29 @@
+package com.danilovfa.cryptocurrencies.data.local.mapper
+
+import com.danilovfa.cryptocurrencies.data.local.model.CryptocurrencyItemEntity
+import com.danilovfa.cryptocurrencies.utils.Mapper
+import com.danilovfa.cryptocurrencies.domain.model.CryptocurrencyItem
+
+class CryptocurrencyItemEntityMapper : Mapper<CryptocurrencyItemEntity, CryptocurrencyItem> {
+    override fun fromEntity(entity: CryptocurrencyItemEntity): CryptocurrencyItem {
+        return CryptocurrencyItem(
+            id = entity.itemId,
+            name = entity.name,
+            symbol = entity.symbol,
+            price = entity.price,
+            marketCap = entity.marketCap,
+            imageUrl = entity.imageUrl
+        )
+    }
+
+    override fun fromDomain(domain: CryptocurrencyItem): CryptocurrencyItemEntity {
+        return CryptocurrencyItemEntity(
+            itemId = domain.id,
+            name = domain.name,
+            symbol = domain.symbol,
+            price = domain.price,
+            marketCap = domain.marketCap,
+            imageUrl = domain.imageUrl
+        )
+    }
+}
--- app/src/main/java/com/danilovfa/cryptocurrencies/domain/model/CryptocurrencyDetails.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/domain/model/CryptocurrencyDetails.kt
@@ -1,11 +1,6 @@
 package com.danilovfa.cryptocurrencies.domain.model
 
 data class CryptocurrencyDetails(
-    val id: String,
-    val name: String,
-    val price: Double,
-    val marketCap: Double,
-    val priceChange: Double,
-    val charts: CryptocurrencyChart,
-    val imageUrl: String
+    val coinDetails: CryptocurrencyItem,
+    val charts: CryptocurrencyChart
 )
