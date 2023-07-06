d46dbaac7f0c675c60e472e63b6449fb94f99750
Fyodor Danilov <danilovfa@protonmail.com>
2023-07-06 16:16:33 +0300

    Add navigation arguments

--- app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/DetailsFragment.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/DetailsFragment.kt
@@ -5,20 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.danilovfa.cryptocurrencies.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+    
+    private val args: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/MainFragment.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/MainFragment.kt
@@ -5,24 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.danilovfa.cryptocurrencies.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun navigateToDetailsScreen(coinId: String) {
+        val action = MainFragmentDirections.actionMainFragmentToDetailsFragment(coinId)
+        findNavController().navigate(action)
     }
 }
