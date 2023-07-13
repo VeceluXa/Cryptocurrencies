e269695e4c913af480bb7be12efc37cd90b7add6
Fyodor Danilov <danilovfa@protonmail.com>
2023-07-13 16:30:47 +0300

    Revert deleting Splash Screen API

--- app/src/main/java/com/danilovfa/cryptocurrencies/app/MainActivity.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/MainActivity.kt
@@ -2,18 +2,28 @@ package com.danilovfa.cryptocurrencies.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.danilovfa.cryptocurrencies.R
 import com.danilovfa.cryptocurrencies.app.viewmodel.SplashViewModel
 import com.danilovfa.cryptocurrencies.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val viewModel: SplashViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/danilovfa/cryptocurrencies/app/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/viewmodel/SplashViewModel.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class SplashViewModel: ViewModel() {
+class SplashViewModel(): ViewModel() {
     private val _isLoading = MutableStateFlow(true)
     val isLoading = _isLoading.asStateFlow()
 
