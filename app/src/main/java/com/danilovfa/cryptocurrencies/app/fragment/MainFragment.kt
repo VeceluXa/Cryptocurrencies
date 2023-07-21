6e5e875d82fdf195b60e8dc9e0073795debb0aa8
Fyodor Danilov <danilovfa@protonmail.com>
2023-07-21 16:25:10 +0300

    Add sort dialog

--- app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/MainFragment.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/fragment/MainFragment.kt
@@ -1,24 +1,63 @@
 package com.danilovfa.cryptocurrencies.app.fragment
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.danilovfa.cryptocurrencies.R
 import com.danilovfa.cryptocurrencies.app.MainActivity
 import com.danilovfa.cryptocurrencies.app.viewmodel.MainViewModel
 import com.danilovfa.cryptocurrencies.databinding.FragmentMainBinding
+import com.danilovfa.cryptocurrencies.domain.model.CryptocurrenciesOrder
+import com.danilovfa.cryptocurrencies.utils.extension.showRadioListDialog
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate) {
+class MainFragment : BaseFragment<FragmentMainBinding>(FragmentMainBinding::inflate), MenuProvider {
     private val viewModel: MainViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (requireActivity() as MainActivity).title = getString(R.string.cryptocurrencies)
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun showSortDialog() {
+        val orders = CryptocurrenciesOrder.values()
+        val ordersStrings = orders.map { order ->
+            getString(order.resourceId)
+        }.toTypedArray()
+
+        requireContext().showRadioListDialog(
+            title = R.string.sort,
+            items = ordersStrings,
+            initialSelectedItemId = viewModel.sortOrder.value.ordinal
+        ) { selectedItem ->
+            viewModel.changeSortOrder(orders[selectedItem])
+        }
     }
 
     private fun navigateToDetailsScreen(coinId: String) {
         val action = MainFragmentDirections.actionMainFragmentToDetailsFragment(coinId)
         findNavController().navigate(action)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_toolbar_main, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
+            R.id.menu_sort -> {
+                showSortDialog()
+                true
+            }
+
+            else -> false
+        }
     }
 }
--- app/src/main/java/com/danilovfa/cryptocurrencies/domain/model/CryptocurrenciesOrder.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/domain/model/CryptocurrenciesOrder.kt
@@ -1,5 +1,7 @@
 package com.danilovfa.cryptocurrencies.domain.model
 
+import androidx.annotation.StringRes
+import com.danilovfa.cryptocurrencies.R
 import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_CAPITALIZATION_ASCENDING
 import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_CAPITALIZATION_DESCENDING
 import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_ID_ASCENDING
@@ -7,11 +9,11 @@ import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_ID_DESCEND
 import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_VOLUME_ASCENDING
 import com.danilovfa.cryptocurrencies.utils.Constants.Companion.ORDER_VOLUME_DESCENDING
 
-enum class CryptocurrenciesOrder(val message: String) {
-    CAPITALIZATION_ASCENDING(ORDER_CAPITALIZATION_ASCENDING),
-    CAPITALIZATION_DESCENDING(ORDER_CAPITALIZATION_DESCENDING),
-    VOLUME_ASCENDING(ORDER_VOLUME_ASCENDING),
-    VOLUME_DESCENDING(ORDER_VOLUME_DESCENDING),
-    ID_ASCENDING(ORDER_ID_ASCENDING),
-    ID_DESCENDING(ORDER_ID_DESCENDING)
+enum class CryptocurrenciesOrder(val apiMessage: String, @StringRes val resourceId: Int) {
+    CAPITALIZATION_ASCENDING(ORDER_CAPITALIZATION_ASCENDING, R.string.order_capitalization_ascending),
+    CAPITALIZATION_DESCENDING(ORDER_CAPITALIZATION_DESCENDING, R.string.order_capitalization_descending),
+    VOLUME_ASCENDING(ORDER_VOLUME_ASCENDING, R.string.order_volume_ascending),
+    VOLUME_DESCENDING(ORDER_VOLUME_DESCENDING, R.string.order_volume_descending),
+    ID_ASCENDING(ORDER_ID_ASCENDING, R.string.order_id_ascending),
+    ID_DESCENDING(ORDER_ID_DESCENDING, R.string.order_id_descending)
 }
