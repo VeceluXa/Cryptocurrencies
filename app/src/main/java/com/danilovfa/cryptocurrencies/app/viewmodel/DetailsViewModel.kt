364b663519bdd66854c46a00655986b64ba38542
Fyodor Danilov <danilovfa@protonmail.com>
2023-07-25 15:27:53 +0300

    Move conversions to viewmodel

--- app/src/main/java/com/danilovfa/cryptocurrencies/app/viewmodel/DetailsViewModel.kt
+++ app/src/main/java/com/danilovfa/cryptocurrencies/app/viewmodel/DetailsViewModel.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlin.math.abs
 
 class DetailsViewModel(
     private val detailsUseCase: GetCryptocurrencyDetailsUseCase
@@ -43,5 +44,45 @@ class DetailsViewModel(
                 }
             }
         }
+    }
+
+    fun priceChangedToString(number: Double): String {
+        var formattedNumber = String.format("%.${if (number == 0.0) 0 else 2}f", abs(number))
+        val indexOfDecimal = formattedNumber.indexOf('.')
+        formattedNumber = if (indexOfDecimal >= 0) {
+            val trailingZeros = formattedNumber.substring(indexOfDecimal + 1).count { it == '0' }
+            if (trailingZeros > 2) {
+                String.format("%.${trailingZeros}f", number)
+            } else {
+                formattedNumber
+            }
+        } else {
+            formattedNumber
+        }
+
+        return if (number >= 0)
+            "+$formattedNumber %"
+        else
+            "-$formattedNumber %"
+    }
+
+    fun priceToString(price: Double): String {
+        return "$price $"
+    }
+
+    fun suffixPriceToString(price: Double): String {
+        val suffixes = listOf("", "K", "M", "B", "T")
+        var num = price
+        var suffixIndex = 0
+
+        while (num >= 1000 && suffixIndex < suffixes.lastIndex) {
+            num /= 1000
+            suffixIndex++
+        }
+
+        val formattedString = String.format("%.2f", num)
+        val suffix = suffixes[suffixIndex]
+
+        return "\$ $formattedString $suffix"
     }
 }
